fce998c80c9db4143e2f1e84685717ad8e69ad1d
vietthuan98 <44547968+vietthuan98@users.noreply.github.com>
2018-12-19 21:01:23 +0700

    Add files via upload
    update register.php

--- Sources/backend/register.php
+++ Sources/backend/register.php
@@ -16,30 +16,33 @@
 			include ("connection.php");
 			if (isset($_POST["btn_submit"])) {
 	  			//lấy thông tin từ các form bằng phương thức POST
+				$username =  $_POST["username"];
 	   			$email = $_POST["email"];
 				$password = $_POST["password"];
 	  			//Kiểm tra điều kiện bắt buộc đối với các field không được bỏ trống
-				  if ($password == "" || $email == "") {
-					   echo "<script>alert('Bạn vui lòng nhập đầy đủ thông tin')</script>";
+				  if ($password == "" || $email == "" || $username == "") {
+					   echo "<script>alert('bạn vui lòng nhập đầy đủ thông tin!')</script>";
 	  			}else{
 	  					// Kiểm tra tài khoản đã tồn tại chưa
-	  					$sql="select * from users where email='$email'";
+	  					$sql="select * from users where email='$email' and username = '$username'";
 						$kt=mysqli_query($conn, $sql);
 	 
 						if(mysqli_num_rows($kt)  > 0){
-							echo "<script>alert('Tài khoản đã tồn tại')</script>";
+							echo "<script>alert('Tài khoản đã tồn tại!')</script> ";
 						}else{
 							//thực hiện việc lưu trữ dữ liệu vào db
 		    				$sql = "INSERT INTO users(
+							username,
 							email,
 		    					password
 		    					) VALUES (
+							'$username',
 		    					'$email',
 		    					'$password'
 		    					)";
 						    // thực thi câu $sql với biến conn lấy từ file connection.php
 	   						mysqli_query($conn,$sql);
-					   		echo "<script>alert('Chúc mừng bạn đã đăng ký thành công')</script>";
+					   		echo "<script>alert('Chúc mừng bạn đã đăng ký thành công!')</script>";
 						}
 										    
 						
@@ -51,7 +54,8 @@
             <div class="image-holder" ></div>
             <form method="post" action="register.php">
                 <h2 class="text-center"><strong>Tạo</strong> tài khoản</h2>
-                <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" required></div>
+      			<div class="form-group"><input class="form-control" type="email" name="email" placeholder="Email" required></div>
+				<div class="form-group"><input class="form-control" type="text" name="username" placeholder="Username" required></div>
                 <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Mật khẩu" required></div>
                 <div class="form-group"><input class="form-control" type="password" name="password-repeat" placeholder="Nhập lại mật khẩu" required></div>
                 <div class="form-group">
