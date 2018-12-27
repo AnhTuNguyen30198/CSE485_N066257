de63e786cf5b5a64283878209f51b806171f229f
vietthuan98 <44547968+vietthuan98@users.noreply.github.com>
2018-12-27 22:21:29 +0700

    js and css for admin page

---
+++ Sources/mycss/admin.js
@@ -0,0 +1,45 @@
+var chooseThem = document.getElementById('choose-them');
+var chooseXoa	=	document.getElementById('choose-xoa');
+var chooseSua	=	document.getElementById('choose-sua');
+var chooseView	=	document.getElementById('choose-view');
+
+var them = document.getElementById('them');
+var xoa = document.getElementById('xoa')
+var sua = document.getElementById('sua')
+var view = document.getElementById('view')
+
+if(chooseThem){
+chooseThem.addEventListener('click',function(){
+	them.style.display = "block";
+	xoa.style.display = "none";
+	sua.style.display = "none";
+	view.style.display = "none";
+},false);
+}
+
+if(chooseXoa){
+chooseXoa.addEventListener('click',function(){
+	them.style.display = "none";
+	xoa.style.display = "block";
+	sua.style.display = "none";
+	view.style.display = "none";
+},false);
+}
+
+if(chooseSua){
+chooseSua.addEventListener('click',function(){
+	them.style.display = "none";
+	xoa.style.display = "none";
+	sua.style.display = "block";
+	view.style.display = "none";
+},false);
+}
+
+if(chooseView){
+chooseView.addEventListener('click',function(){
+	them.style.display = "none";
+	xoa.style.display = "none";
+	sua.style.display = "none";
+	view.style.display = "block";
+},false);
+}
