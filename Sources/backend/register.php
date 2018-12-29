1f6f924201eb9d10d32bf136efdf5af60fc791f9
vietthuan98 <44547968+vietthuan98@users.noreply.github.com>
2018-12-29 22:16:18 +0700

    Update register.php

--- Sources/backend/register.php
+++ Sources/backend/register.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/Login-Form-Dark.css">
     <link rel="stylesheet" href="../assets/css/Registration-Form-with-Photo.css">
     <link rel="stylesheet" href="../assets/css/styles.css">
