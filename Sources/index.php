dc374bdba162bb732dc881ef149f7ef8592801ec
vietthuan98 <44547968+vietthuan98@users.noreply.github.com>
2019-01-03 20:57:59 +0700

    Add files via upload

--- Sources/index.php
+++ Sources/index.php
@@ -20,9 +20,9 @@
 					<!-----Begin LeftMenu: Vpop,kpop,usuk----->
 					<nav>
 						<ul>
-							<li class="vpop"><a href=""><img src="img\vpop.jpg" alt="vpop img"></a></li>
-							<li class="kpop"><a href=""><img src="img\kpop.jpg" alt="kpop img"></a></li>
-							<li class="usuk"><a href=""><img src="img\usuk.jpg" alt="usuk img"></a></li>
+							<li class="vpop" id="choose-vpop"><a href="#topbxh" target="_self"><img src="img\vpop.jpg" alt="vpop img"></a></li>
+							<li class="kpop" id="choose-kpop"><a href="#topbxh" target="_self"><img src="img\kpop.jpg" alt="kpop img"></a></li>
+							<li class="usuk" id="choose-usuk"><a href="#topbxh" target="_self"><img src="img\usuk.jpg" alt="usuk img"></a></li>
 						</ul>
 					</nav>
 				</div>
@@ -30,23 +30,65 @@
 
 				<div id="topbxh" class="col-md-3 box1" >	<!-----Top BXH----->
 					<h2>TOP BXH</h2>
-					<div class="ndtopbxh ndbox1">
-						<p>pđây là nội dung</p>
+					<div class="ndtopbxh ndbox1" id="contentbox1">
+						<div class = "boxvpop">
+							<?php 
+								include"backend/songsvpop.php";
+							 ?>
+						 </div>
+
+						 <div class = "boxkpop" style="display: none">
+								 <?php 
+									include"backend/songskpop.php";
+								 ?>
+						 </div>
+
+						 <div class = "boxusuk" style="display: none">
+								  <?php 
+									include"backend/songsusuk.php";
+								 ?>
+						 </div>
+							
 					</div>
 				</div>
 
 				<div id="likedmusic" class="col-md-3 box1">	<!-----The most favorite songs----->
 					<h2>Được yêu thích</h2>
-					<div class="ndlikedmusic ndbox1">	
-						<p>Đây là nội dung</p>
+					<div class="ndlikedmusic ndbox1" id="contentbox2">	
+						<div class = "boxvpop">
+							
+						 </div>
+
+						 <div class = "boxkpop" style="display: none">
+							
+						 </div>
+
+						 <div class = "boxusuk" style="display: none">
+								 
+						 </div>
 					</div>
 				</div>
 
 				<div id="singer" class="col-md-3 box1">	<!-----Outstanding singer----->
 					<h2>Nghệ sĩ nổi bật</h2>
-					<div class="ndsinger ndbox1">
-						<p>Đây là nội dung</p>
-					</div>
+					<div class="ndsinger ndbox1" id="contentbox3">
+						<div class = "boxvpop">
+							<?php 
+								include"backend/singervpop.php";
+							 ?>
+						 </div>
+
+						 <div class = "boxkpop" style="display: none">
+							<?php 
+								include"backend/singerkpop.php";
+							 ?>	
+						 </div>
+
+						 <div class = "boxusuk" style="display: none">
+							<?php 
+								include"backend/singerusuk.php";
+							 ?>	
+						 </div>
 				</div>
 			</div>
 		</div>
@@ -74,28 +116,36 @@
 					<div id="jazz" class="box2">
 				 		<h2>Jazz</h2>
 				 		<div id="ndjazz" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/jazzsong.php"
+				 			?>
 				 		</div>
 					</div>
 
 					<div id="pop" class="box2">
 						<h2>Pop</h2>
 				 		<div id="ndpop" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/popsong.php"
+				 			?>
 				 		</div>
 					</div>
 
 					<div id="rock" class="box2">
 						<h2>Rock</h2>
 				 		<div id="ndrock" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/rocksong.php"
+				 			?>
 				 		</div>			
 					</div>
 
 					<div id="blue" class="box2">
 						<h2>Blue</h2>
 				 		<div id="ndblue" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/bluesong.php"
+				 			?>
 				 		</div>
 					</div>
 				</div>		<!-----End Row2----->
@@ -106,28 +156,36 @@
 					<div id="soul" class="box2">
 						<h2>Soul</h2>
 				 		<div id="ndsoul" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/soulsong.php"
+				 			?>
 				 		</div>
 					</div>	
 
 					<div id="hiphop" class="box2">
 						<h2>HipHop</h2>
 				 		<div id="ndhiphop" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/hiphopsong.php"
+				 			?>
 				 		</div>
 					</div>
 
 					<div id="folk" class="box2">
 						<h2>Folk</h2>
 				 		<div id="ndfolk" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/folksong.php"
+				 			?>
 				 		</div>		
 					</div>
 
 					<div id="latin" class="box2">
 				 		<h2>Latin</h2>
 				 		<div id="ndlatin" class="ndbox2">
-				 			<p>Đây là nội dung</p>
+				 			<?php
+				 				include "backend/latinsong.php"
+				 			?>
 				 		</div>
 					</div>
 				</div>
